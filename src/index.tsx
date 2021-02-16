282297f0a9d8f34f541b6054b3f9fe0af6aa3b00
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-02-16 21:37:35 +0300

    add mobx

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './components/app';
+import { UserContext } from "./contexts/user-context";
+import { userStore } from "./stores/user";
 
 ReactDOM.render(
   <React.StrictMode>
+    <UserContext.Provider value={userStore}>
       <Router>
         <App />
       </Router>
+    </UserContext.Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
